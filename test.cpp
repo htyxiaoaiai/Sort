3946f506c5a0bc933fe18457bd48692dbed378b1
htyxiaoaiai <htyxiaoaiai1314@gmail.com>
2016-06-19 23:10:37 +0800

    测试文件

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include"Sort.h"
+
+int main()
+{
+	//TestInsertSort();
+	//TestHillSort();
+	//TestSelectSort();
+	//TestSelectSortOP();
+	//TestHeapSort();
+	//TestBubbleSort();
+	//TestQuickSort();
+	//TestGetMidNum();
+	//TestRadixSort();
+	TestMergeSort();
+	getchar();
+	return 0;
+}
